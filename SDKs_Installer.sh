44e2ccdb6bee2c99c5e83aa5e3b09e7d495c61c8
Nguyễn Minh Thọ <69706369+TobyyThePro@users.noreply.github.com>
2020-08-17 17:15:04 +0700

    Update SDKs_Installer.sh

--- SDKs_Installer.sh
+++ SDKs_Installer.sh
@@ -8,6 +8,7 @@ if [ "$1" == "" ]; then
     echo "Supported SDKs are:"
     echo "<+> 12.4"
     echo "<+> 13.1"
+    echo "<+> 13.4"
     echo "To use type: SDKs_Installer.sh <SDKs version>"
 elif [ "$1" == "12.4" ]; then
     echo "Downloading..."
@@ -16,6 +17,10 @@ elif [ "$1" == "12.4" ]; then
 elif [ "$1" == "13.1" ]; then
     echo "Downloading..."
     curl -o iPhoneOS13.1.sdk.zip http://resources.airnativeextensions.com/ios/iPhoneOS13.1.sdk.zip
+    echo "Done!"
+elif [ "$1" == "13.4" ]; then
+    echo "Downloading..."
+    curl -o iPhoneOS13.1.sdk.zip http://resources.airnativeextensions.com/ios/iPhoneOS13.4.sdk.zip
     echo "Done!"
 else
     echo "SDKs version is not support!"
