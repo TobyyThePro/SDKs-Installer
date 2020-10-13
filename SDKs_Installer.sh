0f4910f0e626666ab103918cd2fee771529595b8
Nguyen Minh Tho <toby@pop-os.localdomain>
2020-10-13 17:48:36 +0700

    Update SDKs_Installer.sh

--- SDKs_Installer.sh
+++ SDKs_Installer.sh
@@ -1,4 +1,4 @@
-echo "------======+++ SDKS INSTALLER v1.1 +++======------"
+echo "------======+++ SDKS INSTALLER v1.2 +++======------"
 echo "                         by TobyyThePro"
 echo "GitHub: https://github.com/TobyyThePro"
 echo "Twitter: https://twitter.com/TobyyThePro"
@@ -9,6 +9,7 @@ if [ "$1" == "" ]; then
     echo "[+] 12.4"
     echo "[+] 13.1"
     echo "[+] 13.4"
+    echo "[+] 14.0"
     echo "To use type: SDKs_Installer.sh <SDKs version>"
 elif [ "$1" == "12.4" ]; then
     echo "Downloading..."
@@ -21,6 +22,10 @@ elif [ "$1" == "13.1" ]; then
 elif [ "$1" == "13.4" ]; then
     echo "Downloading..."
     curl -o iPhoneOS13.1.sdk.zip http://resources.airnativeextensions.com/ios/iPhoneOS13.4.sdk.zip
+    echo "Done!"
+elif [ "$1" == "14.0" ]; then
+    echo "Downloading..."
+    curl -o iPhoneOS14.0.sdk.zip http://resources.airnativeextensions.com/ios/iPhoneOS14.0.sdk.zip
     echo "Done!"
 else
     echo "SDKs version is not support!"
